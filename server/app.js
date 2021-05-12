2fbf074d110ba9dec1ccbffa00a9e9312cbe1438
feenatian <feenatian@gmail.com>
2021-05-13 00:33:03 +1000

    feature - get all tickets and test case within mock value

--- server/app.js
+++ server/app.js
@@ -1,8 +1,36 @@
 const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+const bodyParser = require("body-parser");
+
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.get("/tickets", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const response = await axios.get(
+      "https://feenatian.zendesk.com/api/v2/tickets.json?page[size]=100",
+      {
+        auth: {
+          username: username,
+          password: password,
+        },
+      }
+    );
+
+    res.status(200).send(response.data);
+  } catch (err) {
+    console.log(err);
+    res.status(err.response.status).send(err);
+  }
 });
 
 module.exports = app;
