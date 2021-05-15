076a45095ab01ca9ebe8aea817d2845a4dd80719
feenatian <feenatian@gmail.com>
2021-05-15 21:04:03 +1000

    Add on error boundary

--- client/src/errorBoundary/ErorBoundary.js
+++ client/src/errorBoundary/ErorBoundary.js
@@ -1,7 +1,29 @@
 import React from "react";
 
-const ErrorBoundary = () => {
-  return <div>Error Boundary</div>;
-};
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    console.log("console.error();");
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    this.setState({
+      hasError: true,
+    });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
 
 export default ErrorBoundary;
